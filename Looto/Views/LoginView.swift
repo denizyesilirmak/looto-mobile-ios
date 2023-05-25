448c8e4110676a1abce92668a244db3ca5874370
yesilirmak <deniz.yesilirmak@sisal.com>
2023-05-25 21:51:23 +0300

    Add Login Screen

---
+++ Looto/Views/LoginView.swift
@@ -0,0 +1,78 @@
+//
+//  LoginView.swift
+//  Looto
+//
+//  Created by deniz.yesilirmak on 15.08.2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @StateObject var vm = LoginResponseViewModel()
+    @State var email: String = ""
+
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(stops: [
+                Gradient.Stop(color: Color.accentColor, location: 0),
+                Gradient.Stop(color: .black, location: 0.55),
+            ]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                Text("Looto")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.white)
+                    .padding(.bottom, 20)
+                CustomTextField(label: "Email", placeholder: "Enter your email", iconName: "mail", content: $email)
+                    .padding(.bottom, 20)
+                Button(action: {
+                        print("login tapped!")
+                        Task {
+                            await vm.login(email: email)
+                            print("response", vm.loginResponse.data.email)
+                        }
+                    }) {
+                        Text("Login")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding()
+                            .background(Color.accentColor)
+                            .cornerRadius(10.0)
+                            .padding(.bottom, 24)
+                            .padding(.top, 20)
+                    }
+                NavigationLink(
+                    destination: RegisterView(),
+                    label: {
+                        Text("Not a member? Sign up here!")
+                            .font(.system(size: 14, weight: .bold, design: .rounded))
+                            .foregroundColor(.white)
+                    })
+                Spacer()
+                HStack {
+                    Image("LogoHorizontal")
+                        .resizable()
+                        .frame(width: 130, height: 42)
+                        .padding(.bottom, 20)
+                }
+            }
+            .padding()
+        }
+        .background(
+            NavigationLink(
+                destination: OtpView(numberOfFields: 4),
+                isActive: $vm.isOtpRecieved,
+                label: {
+                    EmptyView()
+                })
+        )
+    }
+    
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
