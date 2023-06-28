56a6ba4f3fe215918980d30bba5c9e54661e74d7
yesilirmak <deniz.yesilirmak@sisal.com>
2023-06-28 18:51:23 +0300

    Add Cities model

---
+++ Looto/Models/CityModel.swift
@@ -0,0 +1,28 @@
+//
+//  CityModel.swift
+//  Looto
+//
+//  Created by deniz.yesilirmak on 5.11.2023.
+//
+
+import Foundation
+
+struct CityItem: Codable, Identifiable {
+    let _id: String
+    let code: Int
+    let name: String
+    let latitude: String
+    let longitude: String
+    let population: Int
+    let region: String
+    let __v: Int
+    var id: String {
+        _id
+    }
+}
+
+struct CityResponse: Codable {
+    let data: [CityItem]
+    let success: Bool
+    let records: Int
+}
