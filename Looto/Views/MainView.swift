6231f18ac8cf76a88c7b4209d00a2d52fefc0048
yesilirmak <deniz.yesilirmak@sisal.com>
2023-10-02 19:51:23 +0300

    Add Main view

---
+++ Looto/Views/MainView.swift
@@ -0,0 +1,43 @@
+//
+//  HomeView.swift
+//  Looto
+//
+//  Created by deniz.yesilirmak on 6.11.2023.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct MainView: View {
+    @StateObject var vm = BalanceViewModel()
+    
+    var body: some View {
+        TabView{
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+                .toolbarBackground(
+                                .blue, for: .navigationBar, .tabBar)
+            ResultView()
+                .tabItem {
+                    Label("Result", systemImage: "magnifyingglass")
+                }
+                .toolbarBackground(
+                                .blue, for: .navigationBar, .tabBar)
+            ProfileView(vm: vm)
+                .tabItem {
+                    Label("Profile", systemImage: "person")
+                }
+                .toolbarBackground(
+                                .blue, for: .navigationBar, .tabBar)
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
