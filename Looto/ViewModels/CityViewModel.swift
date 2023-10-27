94d19b4b7179c17c309bd74a347dc7be96c50064
yesilirmak <deniz.yesilirmak@sisal.com>
2023-10-27 19:51:23 +0300

    Update city view model

---
+++ Looto/ViewModels/CityViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  CityViewModel.swift
+//  Looto
+//
+//  Created by deniz.yesilirmak on 15.08.2023.
+//
+
+import Foundation
+
+@MainActor
+
+class CitiesViewModel: ObservableObject {
+    
+    @Published var cities: [CityItem] = []
+    @Published var errorMessage = ""
+    @Published var hasError = false
+    
+    func getCities() async {
+        guard let data = try? await APIService().getCities() else {
+            self.cities = []
+            self.hasError = true
+            self.errorMessage = "Server Error"
+            return
+        }
+        
+        self.cities = data.data
+    }
+    
+}
