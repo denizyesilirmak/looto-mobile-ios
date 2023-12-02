80935e57f449da57f88d943490ae74ad608ae298
yesilirmak <deniz.yesilirmak@sisal.com>
2023-12-02 19:41:17 +0300

    Add ticket code check design

--- Looto/Views/Result/ResultView.swift
+++ Looto/Views/Result/ResultView.swift
@@ -10,6 +10,19 @@ import Foundation
 import SwiftUI
 
 struct ResultView: View {
+    @State var ticketCode: String = ""
+
+    func checkTicket() {
+        print("check ticket")
+        if(ticketCode.count == 0) {
+            let alert = UIAlertController(title: "Error", message: "Please enter a ticket code", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+                print("tapped ok")
+            }))
+            UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: nil)
+        }
+    }
+
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(stops: [
@@ -17,7 +30,22 @@ struct ResultView: View {
                 Gradient.Stop(color: .black, location: 0.55),
             ]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
             VStack {
-
+                TextField("Ticket Code", text: $ticketCode)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .padding(.horizontal, 20)
+                    .padding(.bottom, 20)
+                Button(action: {
+                    checkTicket()
+                }, label: {
+                    Text("Check")
+                        .foregroundColor(.white)
+                        .font(.system(size: 20, weight: .bold, design: .rounded))
+                        .frame(width: 130, height: 50)
+                        .background(Color.accentColor)
+                        .cornerRadius(10)
+                })
             }
             .padding()
         }
